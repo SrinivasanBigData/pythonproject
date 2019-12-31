8fe3aab99a838c0ce962c64df782ffc5a181529d
Srinivasan <eceseenu19898.3@gmail.com>
2019-12-31 10:07:21 +0530

    Update package.scala

--- package.scala
+++ package.scala
@@ -1,15 +1,11 @@
 
 package com.art
 import scala.util.{ Try, Success, Failure }
-import java.sql.ResultSet
-import java.io.OutputStream
-import java.sql.Connection
-import scala.collection.immutable.IndexedSeq
-import java.sql.Statement
+import java.sql.{ ResultSet, Connection, Statement, DriverManager }
 import java.util.Properties
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.Path
+import java.nio.file.{ Files, Paths, Path }
+import java.io.File
+import scala.collection.immutable.IndexedSeq
 
 package object support {
   private[art] def cleanly[A, B](resource: A)(cleanup: A => Unit)(doWork: A => B): Try[B] = {
@@ -35,6 +31,13 @@ package object support {
     prop.asScala.toMap
   }
 
+  implicit def stringClean(value: Option[String]): String = value.map(_.trim).getOrElse("")
+
+  implicit def stringToSeqTuple(parameter: Option[String]): Seq[Tuple2[String, String]] =
+    parameter.split(",")
+      .map(s => s.split(":")).
+      map { case Array(f1, f2) => (f1, f2) }
+
   private[art] trait EnvConstant {
     val url_cons = "db.url"
   }
@@ -44,7 +47,13 @@ package object support {
     val url: String
     val user: String
     val pass: String
-    def getConnection: Connection = null
+
+    @throws[Exception]
+    def getConnection: Connection = {
+      Class.forName(driver)
+      DriverManager.getConnection(url, user, pass)
+    }
+
   }
 
   private[art] case class OutputResult(
@@ -68,13 +77,63 @@ package object support {
       val (d1, d2) = executeSql(sql)
       OutputResult(d1, d2)
     }
+
+    def isExist(Sql: String): Boolean = {
+      false
+    }
   }
 
-  private[art] implicit class ResultSetToExcel(rs: ResultSet)(implicit val sheetname: String) {
-    def getSheetName(): String = sheetname
-    def generateOut(output: OutputStream): String = {
-      """"""
+  private[art] implicit class OutputResultToExcel(rs: OutputResult)(implicit val sheetName: String) {
+    import org.apache.poi.hssf.usermodel.HSSFWorkbook
+    import org.apache.poi.hssf.usermodel.HSSFFont
+    import org.apache.poi.ss.util.CellUtil
+    import org.apache.poi.ss.usermodel.IndexedColors
+
+    private lazy val workbook = new HSSFWorkbook()
+    private lazy val sheet = workbook.createSheet(sheetName)
+    private lazy val boldFont = {
+      val font = workbook.createFont()
+      font.setFontName(HSSFFont.FONT_ARIAL)
+      font.setFontHeightInPoints(10)
+      font.setColor(IndexedColors.BLACK.getIndex())
+      font.setBold(true)
+      font
     }
+
+    private lazy val defaultFont = {
+      val font = workbook.createFont()
+      font.setFontName(HSSFFont.FONT_ARIAL)
+      font.setFontHeightInPoints(10)
+      font.setColor(IndexedColors.BLACK.getIndex())
+      font.setBold(false)
+      font
+    }
+    private lazy val style = {
+      val style = workbook.createCellStyle()
+      style
+    }
+    def generateOut(path: String): File = {
+      val row = sheet.createRow(0)
+      style.setFont(boldFont)
+      row.setRowStyle(style)
+      for ((x, i) <- rs.columns.view.zipWithIndex) {
+        CellUtil.createCell(row, i, x)
+      }
+      for ((a, i) <- rs.results.toSeq.view.zipWithIndex) {
+        var newRow = sheet.createRow(i + 1)
+        style.setFont(defaultFont)
+        newRow.setRowStyle(style)
+        for ((x, i) <- a.view.zipWithIndex) {
+          CellUtil.createCell(row, i, x)
+        }
+      }
+      val outFile = new File(path)
+      cleanly(workbook)(_.close())(w => {
+        w.write(outFile)
+      }).get
+      outFile
+    }
+
   }
 
   object MailObject {
